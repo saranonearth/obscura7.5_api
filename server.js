4af27bd87cf4b294649b0ee3a3cc913250bfbbd3
saranonearth <saran.suresh@outlook.com>
2019-10-31 01:52:48 +0530

    graphql setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const { ApolloServer } = require("apollo-server");
+require("dotenv").config();
+const typeDefs = require("./graphql/typedef");
+const mongoose = require("mongoose");
+const resolvers = require("./graphql/resolvers");
+const PORT = 4000 || process.env.PORT;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+mongoose
+  .connect(process.env.MONGOURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Database Connected");
+    server
+      .listen({ port: PORT })
+      .then(res => console.log(`Server running at ${res.url}`))
+      .catch(err => console.log("Database connection err", err));
+  });
